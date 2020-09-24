3b5d6090b42e39d7cbb6bcb76ad1a11eae6b6e2f
Benoit Lamonica <ben33127@gmail.com>
2020-09-24 11:28:15 +0200

    Added Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,35 @@
 const GulpClient = require("gulp");
-GulpClient.task('test', () => {
-    return console.log('COucou');
+const Plug = require('gulp-load-plugins')();
+const bs = require('browser-sync').create();
+
+console.log(Plug);
+
+GulpClient.task('html-dev', () => {
+    return GulpClient.src('./src/index.html')
+    .pipe(GulpClient.dest('./dist/index.html'));
 });
+
+GulpClient.task('css-dev', () => {
+    return GulpClient.src('./src/sass/*.scss')
+    .pipe(Plug.sass().on('error', Plug.sass.logError))
+    .pipe(GulpClient.dest('./dist/css/'))
+    .pipe(bs.stream());
+})
+
+
+GulpClient.task('clean-dist', () => {
+    return GulpClient.src("./dist/**/*")
+    .pipe(Plug.clean());
+});
+
+GulpClient.task('serve', () => {
+    bs.init({
+        server : './src'
+    })
+    GulpClient.watch("src/sass/*.scss", GulpClient.series('css-dev'));
+    GulpClient.watch("src/*.html", GulpClient.series('html-dev')).on('change', bs.reload);
+});
+
+GulpClient.task('watch', GulpClient.series('serve', 'css-dev'));
+
+GulpClient.task('default', GulpClient.series('watch'));
