1aba136a13ba797e27cd37143aa00353554a1f5d
Félix Bouveret <bouveret.felix@gmail.com>
2020-10-01 11:51:00 +0200

    Js app first draft

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,18 @@
 //Write code here
 let hello = "hey";
+
+const blocks = document.querySelectorAll(".rootArbo li");
+const lists = document.querySelectorAll(".rootArbo ul");
+let listItems = [];
+console.log(lists);
+
+lists.forEach((list) => {
+  let item = list.children;
+  listItems.push(item);
+  // let line = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  // line.setAttribute("width", "100");
+  // line.setAttribute("height", "100");
+  // block.appendChild(line);
+});
+console.log(lists);
+console.log(listItems);
