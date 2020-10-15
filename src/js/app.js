648bb04780a1b054f72f2277e02e3c289d3bf54a
Félix Bouveret <bouveret.felix@gmail.com>
2020-10-15 11:40:46 +0200

    Try js arbo

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,58 @@
 //Write code here
-let hello = "hey";
-
 const blocks = document.querySelectorAll(".rootArbo li");
 const lists = document.querySelectorAll(".rootArbo ul");
 let listItems = [];
-console.log(lists);
 
 lists.forEach((list) => {
-  let item = list.children;
-  listItems.push(item);
-  // let line = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  // line.setAttribute("width", "100");
-  // line.setAttribute("height", "100");
-  // block.appendChild(line);
+  let items = list.children;
+  listItems.push({body:list, children: items });
 });
-console.log(lists);
-console.log(listItems);
+
+listItems.forEach(list => {
+  const listBodyPosition = list.body.getBoundingClientRect()
+  // console.log(listBodyPosition)
+
+  for (let itemIndex = 1; itemIndex <= list.children.length; itemIndex++) {
+    const item = list.children[itemIndex -1]
+
+    const listItemPosition = item.getBoundingClientRect()
+
+    const svgBoxWidth = listBodyPosition.width - listItemPosition.width + 8
+    const svgBoxHeigth = listBodyPosition.height /2 - listItemPosition.height /2
+    // const svgBoxTop = listBodyPosition.top + (listItemPosition.height /2)  + listItemPosition.height * (itemIndex - 1)
+    const svgBoxLeft = listBodyPosition.left
+    let svgBoxTop
+    
+    if(itemIndex < list.children.length/2) {
+       svgBoxTop = listBodyPosition.top + (listItemPosition.height /2) + listItemPosition.top - listBodyPosition.top
+    } else {
+       svgBoxTop = listBodyPosition.top - (listItemPosition.height /2) + listItemPosition.top - listBodyPosition.top
+    }
+
+    console.log(listItemPosition.top)
+
+    let svgBox = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    
+    svgBox.setAttribute("position", "absolute");
+    svgBox.setAttribute("width", svgBoxWidth);
+    svgBox.setAttribute("height", svgBoxHeigth);
+    svgBox.setAttribute("style", `top: ${svgBoxTop}px; left: ${svgBoxLeft}px`);
+    
+    item.appendChild(svgBox);
+
+    let viewBox = '0 0 ' + svgBox.getBoundingClientRect().width + ' ' + svgBox.getBoundingClientRect().height
+    svgBox.setAttribute('viewBox', viewBox)
+
+    let newLine = document.createElementNS(svgBox, 'line')
+      newLine.setAttributeNS(null, 'x1', svgBoxWidth)
+      newLine.setAttributeNS(null, 'y1', svgBoxHeigth)
+      newLine.setAttributeNS(null, 'x2', 0)
+      newLine.setAttributeNS(null, 'y2', 0)
+      newLine.setAttributeNS(null, 'style', 'stroke: #FFFFFF; stroke-width: 2')
+      svgBox.append(newLine)
+
+    console.log(item)
+    console.log(listItemPosition)
+  }
+});
+
