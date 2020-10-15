8fdcac4f5606848344d8c40515e2ae30e382e55b
Félix Bouveret <bouveret.felix@gmail.com>
2020-10-15 11:43:06 +0200

    Fix rebase

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Create SVG background
 
 var svgNS = "http://www.w3.org/2000/svg";
@@ -62,23 +61,3 @@ window.onresize = () => {
     setSvgBg(svgBg);
 }
 
-=======
-//Write code here
-let hello = "hey";
-
-const blocks = document.querySelectorAll(".rootArbo li");
-const lists = document.querySelectorAll(".rootArbo ul");
-let listItems = [];
-console.log(lists);
-
-lists.forEach((list) => {
-  let item = list.children;
-  listItems.push(item);
-  // let line = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  // line.setAttribute("width", "100");
-  // line.setAttribute("height", "100");
-  // block.appendChild(line);
-});
-console.log(lists);
-console.log(listItems);
->>>>>>> Js app first draft
